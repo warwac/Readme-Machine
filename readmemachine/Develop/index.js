bbe91349369bf472ab57335ed27e0bf28695c10f
Wac <warrenaclark@icloud.com>
2022-07-03 01:45:43 -0400

    upload beginning questions and added node packages

--- readmemachine/Develop/index.js
+++ readmemachine/Develop/index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { questions } = require('statuses');
 
 // TODO: Create an array of questions for user input
 const questions = [];
+    [
+        {
+            type: 'input',
+            message:"What is your Project Title?",
+            name:'Title',
+            validate: (value)=>{if(value){return true} else {return 'Requires Input to Proceed'}}
+        }
+    ]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
